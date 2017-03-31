dc936e9f6f26f35656a46460db1e1f15f474ecc8
DuongKK <DuongKK@DESKTOP-KB19RNK>
2017-03-31 12:36:14 +0700

    Add action focus my location on map

--- Naxam.Mapbox.Forms/MapView.cs
+++ Naxam.Mapbox.Forms/MapView.cs
@@ -1,4 +1,8 @@
 ﻿
+using System;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
 using Xamarin.Forms;
 
 namespace Naxam.Mapbox.Forms
@@ -10,15 +14,68 @@ namespace Naxam.Mapbox.Forms
         public double Long { get; set; }
     }
 
+    public class PositionChangeEventArgs : EventArgs
+    {
+        private Position _newPosition;
+
+        public PositionChangeEventArgs(Position newPosition)
+        {
+            NewPosition = newPosition;
+        }
+
+        public Position NewPosition
+        {
+            [CompilerGenerated]
+            get;
+            [CompilerGenerated]
+            private set;
+        }
+    }
+
+    
     public class MapView : View
     {
+
+        public static readonly BindableProperty FocusPositionProperty = BindableProperty.Create(
+           nameof(IsTouchInMap),
+           typeof(bool),
+           typeof(MapView),
+           default(bool),
+        BindingMode.TwoWay
+       //          null,
+       //          ((bindable, newValue, oldValue) =>
+       //          {
+       //              ((MapView)bindable).OnMapCenterChange(bindable,(Position)newValue, (Position)oldValue);
+       //          })
+
+       );
+
+
+        public bool IsTouchInMap
+        {
+            get
+            {
+                return (bool)GetValue(FocusPositionProperty);
+            }
+            set { SetValue(FocusPositionProperty, value); }
+        }
+
+
         public static BindableProperty CenterProperty = BindableProperty.Create(
             nameof(Center),
             typeof(Position),
             typeof(MapView),
             default(Position),
-            defaultBindingMode: BindingMode.OneWay
+         BindingMode.TwoWay
+//          null,
+//          ((bindable, newValue, oldValue) =>
+//          {
+//              ((MapView)bindable).OnMapCenterChange(bindable,(Position)newValue, (Position)oldValue);
+//          })
+
         );
+
+
         public Position Center
         {
             get
@@ -27,7 +84,52 @@ namespace Naxam.Mapbox.Forms
             }
             set { SetValue(CenterProperty, value); }
         }
+      public  void OnMapCenterChange(BindableObject bindable, Position nePosition,Position olPosition)
+        {
+            if (ReferenceEquals(nePosition, olPosition))
+            {
+                MapCenterHandler.Invoke(this,new PositionChangeEventArgs(nePosition));
+            }
+        }
 
+        public EventHandler MapCenterHandler { get; set; }
+
+
+
+
+        public static BindableProperty UserLocationProperty = BindableProperty.Create(
+            nameof(UserLocation),
+            typeof(Position),
+            typeof(MapView),
+            default(Position),
+            defaultBindingMode: BindingMode.TwoWay
+        );
+        public Position UserLocation
+        {
+            get
+            {
+                return (Position)GetValue(UserLocationProperty);
+            }
+            set { SetValue(UserLocationProperty, value); }
+        }
+
+
+
+       /* public static BindableProperty FocusLocationProperty = BindableProperty.Create(
+            nameof(FocusLocationCommand),
+            typeof(ICommand),
+            typeof(MapView),
+            default(ICommand)
+            );
+
+        public ICommand FocusLocationCommand
+        {
+            get { return (ICommand)GetValue(FocusLocationProperty); }
+            set
+            {
+                SetValue(FocusLocationProperty,(ICommand)value);
+            }
+        }*/
         public double ZoomLevel { get; set; }
     }
 }
