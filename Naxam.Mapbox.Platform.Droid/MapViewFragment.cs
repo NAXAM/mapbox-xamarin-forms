fcab391ae758f71de8204addce1f8994c8dd02e8
Vu Duc Tuyen <tuyen@naxam.net>
2017-04-26 16:44:48 +0700

    Correct event handling with maps

--- Naxam.Mapbox.Platform.Droid/MapViewFragment.cs
+++ Naxam.Mapbox.Platform.Droid/MapViewFragment.cs
@@ -1,17 +1,11 @@
 using System;
-using Android.OS;
 using Android.Runtime;
-using Android.Views;
-using Sdk = Mapbox.MapboxSdk;
-using View = Android.Views.View;
+using Mapbox.MapboxSdk.Maps;
 
 namespace Naxam.Mapbox.Platform.Droid
 {
-    public class MapViewFragment : Android.Support.V4.App.Fragment, Sdk.Maps.IOnMapReadyCallback, View.IOnTouchListener
+    public class MapViewFragment : SupportMapFragment
     {
-        public event EventHandler<MapboxMapReadyEventArgs> MapReady;
-        public event EventHandler<MotionEvent> MapTouched;
-
         public MapViewFragment (IntPtr javaReference, JniHandleOwnership transfer)
             : base (javaReference, transfer)
         {
@@ -19,74 +13,6 @@ namespace Naxam.Mapbox.Platform.Droid
 
         public MapViewFragment () : base ()
         {
-        }
-
-        Sdk.Maps.MapView mapView;
-
-        public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-        {
-            mapView = new Sdk.Maps.MapView (Context);
-            mapView.OnCreate (savedInstanceState);
-            mapView.LayoutParameters = new ViewGroup.LayoutParams (
-                ViewGroup.LayoutParams.MatchParent,
-                ViewGroup.LayoutParams.MatchParent);
-
-            mapView.GetMapAsync (this);
-
-            return mapView;
-        }
-
-        public override void OnStart ()
-        {
-            base.OnStart ();
-            mapView.OnStart ();
-        }
-
-        public override void OnResume ()
-        {
-            base.OnResume ();
-            mapView.OnResume ();
-        }
-
-        public override void OnSaveInstanceState (Bundle outState)
-        {
-            base.OnSaveInstanceState (outState);
-            mapView.OnSaveInstanceState (outState);
-        }
-
-        public override void OnPause ()
-        {
-            base.OnPause ();
-            mapView.OnPause ();
-        }
-
-        public override void OnStop ()
-        {
-            base.OnStop ();
-            mapView.OnStop ();
-        }
-
-        public override void OnDestroy ()
-        {
-            base.OnDestroy ();
-            mapView.OnDestroy ();
-        }
-
-        public override void OnLowMemory ()
-        {
-            base.OnLowMemory ();
-            mapView.OnLowMemory ();
-        }
-
-        public void OnMapReady (Sdk.Maps.MapboxMap p0)
-        {
-            MapReady?.Invoke (this, new MapboxMapReadyEventArgs (p0, mapView));
-        }
-
-        public bool OnTouch (View v, MotionEvent e)
-        {
-            MapTouched?.Invoke (this, e);
-            return false;
         }
     }
 }
---
+++ Naxam.Mapbox.Platform.Droid/MapViewRenderer_MapEvents.cs
@@ -0,0 +1,102 @@
+﻿using Mapbox.MapboxSdk.Annotations;
+using Mapbox.MapboxSdk.Maps;
+using System;
+using System.Linq;
+
+namespace Naxam.Mapbox.Platform.Droid
+{
+    public partial class MapViewRenderer : MapView.IOnMapChangedListener
+    {
+        void AddMapEvents()
+        {
+            map.CameraChange += CameraChanged;
+            map.MarkerClick += MarkerClicked;
+            map.MapClick += MapClicked;
+            map.MyLocationChange += MyLocationChanged;
+
+            mapView.AddOnMapChangedListener(this);
+        }
+
+        void RemoveMapEvents()
+        {
+            map.CameraChange -= CameraChanged;
+            map.MarkerClick -= MarkerClicked;
+            map.MapClick -= MapClicked;
+            map.MyLocationChange -= MyLocationChanged;
+
+            mapView.RemoveOnMapChangedListener(this);
+        }
+
+        void CameraChanged(object o, MapboxMap.CameraChangeEventArgs args)
+        {
+            currentCamera.Lat = args.P0.Target.Latitude;
+            currentCamera.Long = args.P0.Target.Longitude;
+            Element.Center = currentCamera;
+        }
+
+        void MyLocationChanged(object o, MapboxMap.MyLocationChangeEventArgs args)
+        {
+            if (Element.UserLocation == null)
+                Element.UserLocation = new Forms.Position();
+
+            Element.UserLocation.Lat = args.P0.Latitude;
+            Element.UserLocation.Long = args.P0.Longitude;
+        }
+
+        void MapClicked(object o, MapboxMap.MapClickEventArgs args)
+        {
+            Element.IsTouchInMap = false;
+
+            var point = map.Projection.ToScreenLocation(args.P0);
+            var xfPoint = new Xamarin.Forms.Point(point.X, point.Y);
+            var xfPosition = new Forms.Position(args.P0.Latitude, args.P0.Longitude);
+
+            Element.DidTapOnMapCommand?.Execute(new Tuple<Forms.Position, Xamarin.Forms.Point>(xfPosition, xfPoint));
+        }
+        
+        void MarkerClicked(object o, MapboxMap.MarkerClickEventArgs args)
+        {
+            Element.Center.Lat = args.P0.Position.Latitude;
+            Element.Center.Long = args.P0.Position.Longitude;
+            Element.IsMarkerClicked = true;
+
+            var annotationKey = _annotationDictionaries.FirstOrDefault(x => x.Value == args.P0).Key;
+
+            if (Element.CanShowCalloutChecker?.Invoke(annotationKey) == true)
+            {
+                args.P0.ShowInfoWindow(map, fragment.View as MapView);
+            }
+        }
+
+        public void OnMapChanged(int p0)
+        {
+            switch (p0)
+            {
+                case MapView.DidFinishLoadingStyle:
+                    var mapStyle = Element.MapStyle;
+                    if (mapStyle == null
+                        || (!string.IsNullOrEmpty(map.StyleUrl) && mapStyle.UrlString != map.StyleUrl))
+                    {
+                        mapStyle = new Forms.MapStyle(map.StyleUrl);
+                        Element.MapStyle = mapStyle;
+                    }
+                    Element.DidFinishLoadingStyleCommand?.Execute(mapStyle);
+                    break;
+                case MapView.DidFinishRenderingMap:
+                    Element.DidFinishRenderingCommand?.Execute(false);
+                    break;
+                case MapView.DidFinishRenderingMapFullyRendered:
+                    Element.DidFinishRenderingCommand?.Execute(true);
+                    break;
+                case MapView.RegionDidChange:
+                    Element.RegionDidChangeCommand?.Execute(false);
+                    break;
+                case MapView.RegionDidChangeAnimated:
+                    Element.RegionDidChangeCommand?.Execute(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
